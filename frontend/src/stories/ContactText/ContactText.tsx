08c789fe10e707a2d8a88fabd2f557c0cfc13a39
Andrew <andrewbaisden@gmail.com>
2023-02-26 21:27:05 +0000

    updated the contact section with new text

--- frontend/src/stories/ContactText/ContactText.tsx
+++ frontend/src/stories/ContactText/ContactText.tsx
@@ -4,17 +4,18 @@ import './ContactText.css';
 interface ContactTextProps {}
 
 export const ContactText = ({ ...props }: ContactTextProps) => {
-	return (
-		<>
-			<div className="contact-text">
-				<p>
-					At the moment I am in a full-time role however I am still open to talking about other projects or even some
-					light freelance programming or writing jobs. You can reach me here at{' '}
-					<a href="mailto:andrewbaisden@gmail.com">andrewbaisden@gmail.com</a>. I look forward to hearing from you!
-				</p>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="contact-text">
+        <p>
+          I am open to talking about other projects, freelance and writing
+          roles. You can reach me here at{' '}
+          <a href="mailto:andrewbaisden@gmail.com">andrewbaisden@gmail.com</a>.
+          I look forward to hearing from you!
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default ContactText;
