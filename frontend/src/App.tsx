a43c2ea6b05eed3580ae039e9c8344aa71cbe4c8
Andrew <andrewbaisden@gmail.com>
2024-03-12 12:31:06 +0000

    theme adjustments for the footer and added 1st formations logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,119 +13,150 @@ import FooterText from './stories/FooterText/FooterText';
 import { useTheme } from '../src/context/ThemeContext';
 
 const App = () => {
-	const { activeTheme } = useTheme();
+  const { activeTheme } = useTheme();
 
-	const homeRef = useRef(null);
-	const aboutRef = useRef(null);
-	const scoialMediaRef = useRef(null);
-	const skillsRef = useRef(null);
-	const contactRef = useRef(null);
+  const homeRef = useRef(null);
+  const aboutRef = useRef(null);
+  const scoialMediaRef = useRef(null);
+  const skillsRef = useRef(null);
+  const contactRef = useRef(null);
 
-	return (
-		<>
-			<div className="container">
-				<div>
-					<div className="hero-container" ref={homeRef}>
-						<Hero />
-						<div className="hero-header-container">
-							<Header
-								homeRef={homeRef}
-								aboutRef={aboutRef}
-								scoialMediaRef={scoialMediaRef}
-								skillsRef={skillsRef}
-								contactRef={contactRef}
-							/>
-						</div>
-						<div className="hero-bio-container">
-							<HeroProfile />
-						</div>
-					</div>
-					<main>
-						<div ref={aboutRef}>
-							<section className="header-section">
-								<a
-									href="/#about"
-									rel="noopener noreferrer"
-									className={activeTheme === 'light' ? 'header-title-light' : 'header-title-dark'}
-								>
-									About
-								</a>
-							</section>
-							<section
-								className={`about-section ${
-									activeTheme === 'light' ? 'section-border-light-theme' : 'section-border-dark-theme'
-								}`}
-							>
-								<div>
-									<AboutProfileText />
-								</div>
-								<div>
-									<AboutProfileImage />
-								</div>
-							</section>
-						</div>
-						<div ref={scoialMediaRef}>
-							<section className="header-section">
-								<a
-									href="/#socialmedia"
-									rel="noopener noreferrer"
-									className={activeTheme === 'light' ? 'header-title-light' : 'header-title-dark'}
-								>
-									Social Media
-								</a>
-							</section>
-							<section
-								className={`socialmedia-section ${
-									activeTheme === 'light' ? 'section-border-light-theme' : 'section-border-dark-theme'
-								}`}
-							>
-								<SocialMediaCarousel />
-							</section>
-						</div>
-						<div ref={skillsRef}>
-							<section className="header-section">
-								<a
-									href="/#skills"
-									rel="noopener noreferrer"
-									className={activeTheme === 'light' ? 'header-title-light' : 'header-title-dark'}
-								>
-									Skills
-								</a>
-							</section>
-							<section
-								className={`skills-section ${
-									activeTheme === 'light' ? 'section-border-light-theme' : 'section-border-dark-theme'
-								}`}
-							>
-								<SkillGroupIcons />
-							</section>
-						</div>
-						<div ref={contactRef}>
-							<section className="header-section">
-								<a
-									href="/#contact"
-									rel="noopener noreferrer"
-									className={activeTheme === 'light' ? 'header-title-light' : 'header-title-dark'}
-								>
-									Contact
-								</a>
-							</section>
-							<section
-								className={`contact-section ${
-									activeTheme === 'light' ? 'section-border-light-theme' : 'section-border-dark-theme'
-								}`}
-							>
-								<ContactText />
-							</section>
-						</div>
-					</main>
-					<footer className={activeTheme === 'light' ? 'footer-light-theme' : 'footer-dark-theme'}>
-						<FooterText />
-					</footer>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="container">
+        <div>
+          <div className="hero-container" ref={homeRef}>
+            <Hero />
+            <div className="hero-header-container">
+              <Header
+                homeRef={homeRef}
+                aboutRef={aboutRef}
+                scoialMediaRef={scoialMediaRef}
+                skillsRef={skillsRef}
+                contactRef={contactRef}
+              />
+            </div>
+            <div className="hero-bio-container">
+              <HeroProfile />
+            </div>
+          </div>
+          <main>
+            <div ref={aboutRef}>
+              <section className="header-section">
+                <a
+                  href="/#about"
+                  rel="noopener noreferrer"
+                  className={
+                    activeTheme === 'light'
+                      ? 'header-title-light'
+                      : 'header-title-dark'
+                  }
+                >
+                  About
+                </a>
+              </section>
+              <section
+                className={`about-section ${
+                  activeTheme === 'light'
+                    ? 'section-border-light-theme'
+                    : 'section-border-dark-theme'
+                }`}
+              >
+                <div>
+                  <AboutProfileText />
+                </div>
+                <div>
+                  <AboutProfileImage />
+                </div>
+              </section>
+            </div>
+            <div ref={scoialMediaRef}>
+              <section className="header-section">
+                <a
+                  href="/#socialmedia"
+                  rel="noopener noreferrer"
+                  className={
+                    activeTheme === 'light'
+                      ? 'header-title-light'
+                      : 'header-title-dark'
+                  }
+                >
+                  Social Media
+                </a>
+              </section>
+              <section
+                className={`socialmedia-section ${
+                  activeTheme === 'light'
+                    ? 'section-border-light-theme'
+                    : 'section-border-dark-theme'
+                }`}
+              >
+                <SocialMediaCarousel />
+              </section>
+            </div>
+            <div ref={skillsRef}>
+              <section className="header-section">
+                <a
+                  href="/#skills"
+                  rel="noopener noreferrer"
+                  className={
+                    activeTheme === 'light'
+                      ? 'header-title-light'
+                      : 'header-title-dark'
+                  }
+                >
+                  Skills
+                </a>
+              </section>
+              <section
+                className={`skills-section ${
+                  activeTheme === 'light'
+                    ? 'section-border-light-theme'
+                    : 'section-border-dark-theme'
+                }`}
+              >
+                <SkillGroupIcons />
+              </section>
+            </div>
+            <div ref={contactRef}>
+              <section className="header-section">
+                <a
+                  href="/#contact"
+                  rel="noopener noreferrer"
+                  className={
+                    activeTheme === 'light'
+                      ? 'header-title-light'
+                      : 'header-title-dark'
+                  }
+                >
+                  Contact
+                </a>
+              </section>
+              <section
+                className={`contact-section ${
+                  activeTheme === 'light'
+                    ? 'section-border-light-theme'
+                    : 'section-border-dark-theme'
+                }`}
+              >
+                <ContactText />
+              </section>
+            </div>
+          </main>
+          {/* <footer className={activeTheme === 'light' ? 'footer-light-theme' : 'footer-dark-theme'}> */}
+          <footer
+            className={
+              activeTheme === 'light'
+                ? 'footer-light-theme'
+                : 'footer-light-theme'
+            }
+          >
+            <FooterText />
+          </footer>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
